6b5afae0b1bacb4934368e458b93e3261565c9e7
Odin Hoff Gardaa <odinhg@gmail.com>
2023-03-14 12:05:13 +0100

    added dataset summary

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Weather prediction from stereo images
 ### Project 2 in INF367A : Topological Deep Learning
 **Odin Hoff Gardå, April 2023**
+
+## Dataset
+
+The dataset consists of 1000 stereo images (one left and one right image). Each image has 3 channels (RGB) with resolution 879x400 (w x h). The possible label values are 'cloudy' (0) and 'sunny' (1). The dataset is perfectly balanced with 500 samples of each label.
+
+![Sunny image](figs/image_2.png)
+![Cloudy image](figs/image_3.png)
+*Figure: Two images (index 2 and 3) from the dataset (left and right view) with labels 'sunny' and 'cloudy'.*
---
+++ data_summary.py
@@ -0,0 +1,24 @@
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+
+X = np.load("./data/X.npy")
+y = np.load("./data/y.npy")
+
+no_images, views, height, width, channels = X.shape
+
+print(f"Number of images: {no_images}")
+print(f"Views per image: {views}")
+print(f"Resolution (w x h): {width} x {height}")
+print(f"Channels: {channels}")
+
+unique, counts = np.unique(y, return_counts=True)
+
+print(f"Unique labels: {unique}")
+print(f"Label counts: {counts}")
+
+font = ImageFont.truetype("./data/font.ttf", 32)
+
+for i in range(10):
+    im = Image.fromarray(np.hstack([X[i,0], X[i,1]]))
+    ImageDraw.Draw(im).text((10,10), y[i], font=font, fill=(255, 255, 0))
+    im.save(f"./figs/image_{i}.png")
